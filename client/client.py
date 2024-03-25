a3fcc216a638372a4a632156195b4907a945ac3a
LennartWinter <120097925+LennartWinter@users.noreply.github.com>
2024-03-25 12:31:35 +0100

    Theme updates

--- client/client.py
+++ client/client.py
@@ -17,16 +17,16 @@ class ChatClientGUI:
         self.sidebar = tk.Frame(master, bg="black")
         self.sidebar.pack(side=tk.LEFT, fill=tk.Y, expand=False)
 
-        self.text_area = tk.Text(master, bg="black", fg="yellow", font=("Arial", 12), cursor="arrow")
+        self.text_area = tk.Text(master, bg="black", fg="white", font=("Arial", 12), cursor="arrow")
         self.text_area.pack(fill=tk.BOTH, expand=True)
 
-        self.input_field = tk.Entry(master, bg="gray", fg="lightgreen", font=("Arial", 12), cursor="tcross")
+        self.input_field = tk.Entry(master, bg="black", fg="white", font=("Arial", 12), cursor="tcross")
         self.input_field.pack(side=tk.LEFT, fill=tk.X, expand=True, ipadx=5, ipady=5)
 
-        self.send_button = tk.Button(master, text="Send", bg="black", fg="yellow", font=("Arial", 12), command=self.send_message)
+        self.send_button = tk.Button(master, text="►", bg="black", fg="lightgreen", font=("Arial", 12), command=self.send_message)
         self.send_button.pack(side=tk.LEFT, fill=tk.X, expand=False)
 
-        self.button = tk.Button(self.sidebar, text="#general", bg="gray", fg="lightgreen", font=("Arial", 8), command=lambda: self.send_message("/join general"))
+        self.button = tk.Button(self.sidebar, text="#general", bg="black", fg="lightgreen", font=("Arial", 8), command=lambda: self.send_message("/join general"))
         self.button.pack(fill=tk.X)
 
         self.input_field.bind("<Return>", self.send_message)
@@ -70,7 +70,7 @@ class ChatClientGUI:
                     for widget in self.sidebar.winfo_children():
                         widget.destroy()
                     for chan in chans:
-                        button = tk.Button(self.sidebar, text=f"#{chan}", bg="gray", fg="lightgreen", font=("Arial", 8), command=lambda chan=chan: self.send_message(f"/join {chan}"))
+                        button = tk.Button(self.sidebar, text=f"#{chan}", bg="black", fg="lightgreen", font=("Arial", 8), command=lambda chan=chan: self.send_message(f"/join {chan}"))
                         button.pack(fill=tk.X)
                 else:
                     self.text_area.insert(tk.END,'\n' +  msg)
