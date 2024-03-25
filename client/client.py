6e415a7a0025180b581cb9acc6789b7d85c873db
LennartWinter <120097925+LennartWinter@users.noreply.github.com>
2024-03-25 09:21:58 +0100

    Turned client into GUI

--- client/client.py
+++ client/client.py
@@ -1,81 +1,70 @@
 import socket
 import threading
-import sys
-import os
 import random
 import time
-import colors
 import datetime
-
-os.system("cls")
+import tkinter as tk
 
 username = "Guest" + str(random.randint(1, 9999))
-password = "pass"
 
 server_ip = "127.0.0.1"
 server_port = 8001
 
-def receive_messages(client):
-    try:
-        while True:
-            data = client.recv(1024)
-            if not data:
-                break
-            msg = data.decode("utf-8")
-            if msg.startswith("/pong"):
-                ping_time = datetime.datetime.now() - datetime.datetime.strptime(' '.join(msg.split(" ")[1:]), "%Y:%m:%d:%H:%M:%S:%f")
-                ping_ms = ping_time.total_seconds() * 1000
-                print(colors.prGreen("Ping: " + str(round(ping_ms)) + " ms"))
-            elif "<<" in msg or ">>" in msg:
-                print(colors.prPurple(msg))
-            elif ("<" in msg and ">" in msg and msg.split("<")[1].split(">")[0]) or (msg[0]=="*"):
-                print(msg)
-            else:
-                print(colors.prYellow(msg))
-    except Exception as e:
-        print(colors.prRed(f"Error: {e}"))
+class ChatClientGUI:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Velocity Client")
 
+        self.text_area = tk.Text(master)
+        self.text_area.pack(fill=tk.BOTH, expand=True)
 
-def run_client(server_ip, server_port):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_ip, server_port))
+        self.input_field = tk.Entry(master)
+        self.input_field.pack(fill=tk.X)
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client,))
-    receive_thread.start()
+        self.input_field.bind("<Return>", self.send_message)
 
-    try:
-        client.send("/motd".encode("utf-8"))
-        time.sleep(0.2)
-        client.send(f"/nick {username}".encode("utf-8"))
-        while True:
-            msg = input()
-            sys.stdout.write("\033[F")
-            if msg == "/clear":
-                os.system("cls")
-            elif msg.startswith("/conn"):
-                server_ip = msg.split(" ")[1].split(":")[0]
-                server_port = int(msg.split(":")[1])
-                return {"ip": server_ip, "port": server_port, "is_running": True}
-            else:
-                client.send(msg.encode("utf-8")[:1024])
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        client.close()
-        print("Connection to server closed")
+        self.connect_to_server()
 
+    def connect_to_server(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((server_ip, server_port))
 
-is_running = True
-while is_running:
-    os.system("cls")
-    print(f"Connecting to {server_ip}:{server_port}")
-    try:
-        output = run_client(server_ip, server_port)
-        is_running = output["is_running"]
-        server_ip = output["ip"]
-        server_port = output["port"]
-    except Exception as e:
-        print(colors.prRed(f"Error: {e}"))
-        server_ip = input("Enter server IP: ")
-        server_port = int(input("Enter server port: "))
-        is_running = True
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.daemon = True
+        receive_thread.start()
+
+        self.send_message(f"/nick {username}")
+
+    def receive_messages(self):
+        try:
+            while True:
+                data = self.client.recv(1024)
+                if not data:
+                    break
+                msg = data.decode("utf-8")
+                if msg.startswith("/pong"):
+                    ping_time = datetime.datetime.now() - datetime.datetime.strptime(' '.join(msg.split(" ")[1:]), "%Y:%m:%d:%H:%M:%S:%f")
+                    ping_ms = ping_time.total_seconds() * 1000
+                    self.text_area.insert(tk.END, '\n'+"Ping: " + str(round(ping_ms)) + " ms")
+                else:
+                    self.text_area.insert(tk.END,'\n' +  msg)
+                self.text_area.see(tk.END)
+        except Exception as e:
+            print(f"Error: {e}")
+
+    def send_message(self, event=None):
+        if not self.input_field.get() and event:
+            self.client.send(event.encode("utf-8")[:1024])
+        msg = self.input_field.get()
+        if msg == "/clear":
+            self.text_area.delete('1.0', tk.END)
+        else:
+            self.client.send(msg.encode("utf-8")[:1024])
+        self.input_field.delete(0, tk.END)
+
+def run_gui():
+    root = tk.Tk()
+    app = ChatClientGUI(root)
+    root.mainloop()
+
+run_gui()
--- client/colors.py
+++
@@ -1,8 +0,0 @@
-def prRed(skk):
-    return "\033[91m{}\033[00m     " .format(skk)
-def prYellow(skk):
-    return "\033[93m{}\033[00m     " .format(skk)
-def prPurple(skk):
-    return "\033[95m{}\033[00m     " .format(skk)
-def prGreen(skk): 
-    return "\033[92m{}\033[00m     " .format(skk)
